9636a1712dfd08b326d74a1d6a90e523e6843eb2
Fauzi <fauzieuy@gmail.com>
2014-05-29 21:54:21 +0700

    Update related_posts.rb

--- _plugins/related_posts.rb
+++ _plugins/related_posts.rb
@@ -1,34 +1,36 @@
 require 'jekyll/post'
 
-module Jekyll
-  module RelatedPostsByTags
-    # Used to remove #related_posts so that it can be overridden
-    def self.included(klass)
-      klass.class_eval do
-        remove_method :related_posts
-      end
+module RelatedPosts
+
+  # Used to remove #related_posts so that it can be overridden
+  def self.included(klass)
+    klass.class_eval do
+      remove_method :related_posts
     end
+  end
 
-    # Calculate related posts.
-    # Returns [<Post>]
-    def related_posts(posts)
-      return [] unless posts.size > 1
-      highest_freq = tag_freq(posts).values.max
-      related_scores = Hash.new(0)
-
-      posts.each do |post|
-        post.tags.each do |tag|
-          if self.tags.include?(tag) && post != self
-            cat_freq = tag_freq(posts)[tag]
-            related_scores[post] += (1+highest_freq-cat_freq)
-          end
+  # Calculate related posts.
+  #
+  # Returns [<Post>]
+  def related_posts(posts)
+    return [] unless posts.size > 1
+    highest_freq = Jekyll::Post.tag_freq(posts).values.max
+    related_scores = Hash.new(0)
+    posts.each do |post|
+      post.tags.each do |tag|
+        if self.tags.include?(tag) && post != self
+          cat_freq = Jekyll::Post.tag_freq(posts)[tag]
+          related_scores[post] += (1+highest_freq-cat_freq)
         end
       end
-
-      sort_related_posts(related_scores)
     end
 
+    Jekyll::Post.sort_related_posts(related_scores)
+  end
+
+  module ClassMethods
     # Calculate the frequency of each tag.
+    #
     # Returns {tag => freq, tag => freq, ...}
     def tag_freq(posts)
       return @tag_freq if @tag_freq
@@ -54,7 +56,11 @@ module Jekyll
     end
   end
 
+end
+
+module Jekyll
   class Post
-    include RelatedPostsByTags
+    include RelatedPosts
+    extend RelatedPosts::ClassMethods
   end
 end
